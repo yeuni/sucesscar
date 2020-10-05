9558649b472b061da51c8b4ad5fd299051b3db41
Yeuni <yeunidev@gmail.com>
2020-10-05 10:16:53 +0300

    develop first commit

---
+++ app/src/main/java/www/wakapaymentscom/yeunithegit/ThirdMyActivity.java
@@ -0,0 +1,14 @@
+package www.wakapaymentscom.yeunithegit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdMyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_my);
+    }
+}
